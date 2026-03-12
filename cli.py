3442fc6c630edde7895afdecc895e651e8f775a6
Nikolai Kirilov <Kirilov.Nikolai@gmail.com>
2026-03-12 17:03:13 +0900

    Make cli non-interactive with arguments only to convert to litematic and dump info/statistics

--- cli.py
+++ cli.py
@@ -1,28 +1,37 @@
-import grabcraft_to_schema as gts
-import blockmodel_avg_mapper as bam
-import json
-import PIL
-from PIL import Image
-import numpy as np
+import sys, json
+from collections import defaultdict
+from grabcraft_to_schema import RenderObject
 
-# Load the block map
-gts.load_block_map("blockmap.csv")
-
-# A demo link that you can try to use:
-# "https://www.grabcraft.com/minecraft/gothic-medieval-church/churches#model3d"
-schem = gts.url_to_render_object_data(input("Enter the link to the build: "))
+# Download render object
+north, url = sys.argv[1:]
+robj = RenderObject(url, north)
 print("Done downloading!\n")
 
-match input("Would you like to save it as\n\t(1) A litematica schematic\n\t(2) RenderObject json\n\t(3) PNG slice\n"):
-    case "1": # Litematica
-        schem = gts.render_object_to_schema(schem)
-        # Save the schem as litematica
-        schem.save(f"{ input('Enter what you want to name the litematic: ') }.litematic")
-    case "2": # RenderObject
-        # Save the schem as render object json
-        with open(f"{ input('Enter what you want to name the Render Object: ') }.json", 'w') as f:
-            f.write(json.dumps(schem.__dict__))
-    case "3": # PNG slice
-        gts.render_object_to_png_slice(schem).save(f"{ input('Enter what you want to name the png slice: ') }.png")
-    case _:
-        print("That's an invalid option!")
+# Convert render object to MC schema
+schem, materials_list = robj.to_schema()
+
+out_name = robj.name.replace(" ", "_")
+
+# Save the schema as litematica
+schem.save(out_name + ".litematic")
+
+# Save the schema statistics
+with open(out_name + ".txt", "w") as f:
+    print(" -= info =-", file=f)
+    print("Name:", robj.name, file=f)
+    print("URL:", robj.url, file=f)
+    print("Facing North:", robj.north, file=f)
+    print("Dimensions:", robj.dims, file=f)
+    print("GrabCraft Tags:", robj.tags, file=f)
+    print("Blocks:", len(robj.blocks), file=f)
+
+    print(" -= blocks =-", file=f)
+    blocks_list = defaultdict(int)
+    for block in robj.blocks:
+        blocks_list[block[0]] += 1
+    for block in sorted(blocks_list):
+        print(f"{blocks_list[block]:6d}", block, file=f)
+
+    print(" -= materials =-", file=f)
+    for material in sorted(materials_list):
+        print(f"{materials_list[material]:6d}", material, file=f)
